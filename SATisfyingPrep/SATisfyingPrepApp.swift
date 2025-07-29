36909ab3fe4b722310c27ea0b9a3b9b3a371051a
orienna <orichan330@gmail.com>
2025-07-29 09:52:50 -0700

    Initial Commit

---
+++ SATisfyingPrep/SATisfyingPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  SATisfyingPrepApp.swift
+//  SATisfyingPrep
+//
+//  Created by Scholar on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SATisfyingPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
