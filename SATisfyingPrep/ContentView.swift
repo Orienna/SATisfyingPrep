dfdaed1e7453c1a3e09ff9c22b9408ec6f4abdc8
Oteiya Chan <oteiyachan@gmail.com>
2025-07-31 10:03:34 -0700

    removed untitled

--- SATisfyingPrep/ContentView.swift
+++ SATisfyingPrep/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+<<<<<<< HEAD
 struct ContentView: View {
   var body: some View {
     NavigationStack {
@@ -38,6 +39,52 @@ struct ContentView: View {
       }//zstack
     }//navigation
   }//body
+=======
+
+struct secondViewFile: View {
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Image("purpleBG") // Replace with the name of your asset
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea() // Make it cover the whole screen
+                VStack {
+                    Image("logo")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .padding(.bottom, 50.0)
+                        .frame(width: 300.0, height: 300.0)
+                        .cornerRadius(20)
+                    
+                    NavigationLink(destination: mainPage()) {
+                        Text("Sign in")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.skyBlue)
+                            .frame(width: 300.0, height: 60.0)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                        
+                    }
+                    .padding()
+                    NavigationLink(destination: mainPage()) {
+                        Text("Log in")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.skyBlue)
+                            .frame(width: 300.0, height: 60.0)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                    }
+                    
+                }
+            }//zstack
+            
+        }//navigation
+    }//body
+    
+>>>>>>> main
 }//struct
 #Preview {
   ContentView()
